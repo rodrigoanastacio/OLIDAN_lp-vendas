94465c4420b4f675fe682346f4467b8bf51fd2d9
Rodrigo Anastacio <rodrigo.anastacio@wundermanthompson.com>
2022-03-01 19:15:50 -0300

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Olidan ateliê | Vestidos sob medidas</title>
+        <meta
+          name="description"
+          content="Olidan Ateliê trabalha com vestidos desenvolvidos para cada noiva, de forma individual, assim como são os sonhos."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
